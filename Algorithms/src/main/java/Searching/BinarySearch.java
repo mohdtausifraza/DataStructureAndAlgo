aa60a3620d21a1fda9c3dc941e9fa642a6bc8e5d
Tausif <49245808+mohdtausifraza@users.noreply.github.com>
2020-10-02 02:00:24 +0530

    BinarySearchRecursive

--- Algorithms/src/main/java/Searching/BinarySearch.java
+++ Algorithms/src/main/java/Searching/BinarySearch.java
@@ -1,3 +1,4 @@
+package Searching;
 /**
  * The Array should be sorted, then only we can perform Binary Search.
  *  This can be solved by DIVIDE AND CONQUER Strategies.
@@ -12,6 +13,8 @@ public class BinarySearch {
      *      if yes return mid
      *      else if key < arr[mid] then , update high=mid-1
      *      else if key > arr[mid] then , update low=mid+1
+     *
+     *      Time Complexity = O(logn)
      */
     public static int binarySearchIterative(int[] arr, int n,int key){
         int low=0,high=n-1,mid;
@@ -28,8 +31,32 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     *  Recursively calls the function till left == right
+     *  Update mid = (left+right)/2;
+     *  if key is smaller than arr[mid] Update right = mid-1;
+     *  if key is greater that arr[mid] Update left = mid+1
+     *
+     *  Time Complexity = O(logn)
+     */
+    public static int binarySearchRecursive(int[] arr, int left, int right,int key){
+        if(left == right){
+            return (arr[left] == key) ?  left :  -1;
+        }
+        int mid = (left+right)/2;
+        if (arr[mid] == key){
+            return mid;
+        }else if (key < arr[mid]){
+            return binarySearchRecursive(arr , left, mid-1, key);
+        }else {
+            return binarySearchRecursive(arr, mid+1, right, key);
+        }
+    }
+
     public static void main(String[] args) {
         int[] arr = {3,6,8,12,14,17,25,29,31,36,42,47,53,55,62,65};
-        System.out.println(binarySearchIterative(arr,16,31));
+        System.out.println(binarySearchIterative(arr,16,12));
+
+        System.out.println(binarySearchRecursive(arr,0,15,12));
     }
 }
