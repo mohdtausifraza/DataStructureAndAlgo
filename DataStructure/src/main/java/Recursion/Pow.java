0b6b0e728378729cd72e83378fa8a3e2262794c2
Tausif <49245808+mohdtausifraza@users.noreply.github.com>
2020-10-23 19:10:04 +0530

    Pow Recursively With less multiplication

--- DataStructure/src/main/java/Recursion/Pow.java
+++ DataStructure/src/main/java/Recursion/Pow.java
@@ -13,7 +13,19 @@ public class Pow {
             return pow(m ,n-1)*m;
         }
     }
+
+    // Second way with less multiplications.
+    public static int powLessMultiplication(int m, int n){
+        if (n==0){
+            return 1;
+        }else if (n % 2 == 0){
+            return powLessMultiplication(m*m ,n/2);
+        }else {
+            return m* powLessMultiplication(m*m ,(n-1)/2);
+        }
+    }
     public static void main(String[] args) {
         System.out.println(pow(2,5));
+        System.out.println(powLessMultiplication(2,5));
     }
 }
