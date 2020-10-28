8e13fde152abda0b8f72cb9fd5b5e43d493c62fe
Tausif <49245808+mohdtausifraza@users.noreply.github.com>
2020-10-29 00:59:45 +0530

    Array_ADT Add and Insert

--- DataStructure/src/main/java/Arrays/Arrays.java
+++ DataStructure/src/main/java/Arrays/Arrays.java
@@ -38,10 +38,55 @@ public class Arrays {
         System.out.println();
     }
 
+    /**
+     * Adding or appending value(x) at the end of the array;
+     * @param value;
+     */
+    public static void add(int value){
+        if (Arrays.length == Arrays.size){
+            System.out.println("Array is full. No new insertion is possible");
+            return;
+        }
+        Arrays.arr[length] = value;
+        length++;
+    }
+
+    /**
+     * Insert the element at the given index
+     * @param index
+     * @param value
+     * Time Complexity : O(n)
+     */
+    public static void insert(int index , int value){
+        if (Arrays.length == Arrays.size){
+            System.out.println("Array is full. Cannot insert at given index");
+            return;
+        }if (index == length){
+            arr[index] = value;
+            length++;
+        }
+        int tempIndex;
+        for (tempIndex=length ; tempIndex > index ; tempIndex--){
+            arr[tempIndex] = arr[tempIndex-1];
+        }
+        arr[tempIndex] = value;
+        length++;
+    }
+
 
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6};
         Arrays array = new Arrays(arr , 10,6);
+        System.out.println("Arrays Length is : "+Arrays.length);
+        System.out.println("Arrays Size is : "+Arrays.size);
+        Arrays.display();
+
+        Arrays.add(7);
+        System.out.println("Arrays Length is : "+Arrays.length);
+        System.out.println("Arrays Size is : "+Arrays.size);
+        Arrays.display();
+
+        Arrays.insert(4, -1);
         System.out.println("Arrays Length is : "+Arrays.length);
         System.out.println("Arrays Size is : "+Arrays.size);
         Arrays.display();
