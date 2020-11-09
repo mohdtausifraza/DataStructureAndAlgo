32cd7b5bc42daa00dd37ddcef79147dc92eb8b56
Tausif <49245808+mohdtausifraza@users.noreply.github.com>
2020-11-09 23:09:37 +0530

    String toggleCase, countVowels, countConsonants.

--- DataStructure/src/main/java/String/MyString.java
+++ DataStructure/src/main/java/String/MyString.java
@@ -36,17 +36,58 @@ public class MyString {
         return new String(array);
     }
 
+    public static String toggleCase(String s){
+        char[] array = s.toCharArray();
+        for (int i =0 ; i < array.length ; i++){
+            int c = array[i];
+            if (c>='a' && c<='z'){
+                array[i] = (char)(array[i]-' ');
+            }else if (c>='A' && c<='Z'){
+                array[i] = (char) (array[i]+' ');
+            }
+        }
+        return new String(array);
+    }
+
+    public static int countVowels(String s) {
+        char[] array = s.toCharArray();
+        int count = 0;
+        for (char c : array){
+            if (c=='a' || c=='e' ||c=='i' || c=='o' ||c=='u' || c=='A' ||c=='E' || c=='I' ||c=='O' || c=='U'){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static int countConsonants(String s) {
+        char[] array = s.toCharArray();
+        int count = 0;
+        for (char c : array){
+            if (!(c=='a' || c=='e' ||c=='i' || c=='o' ||c=='u' || c=='A' ||c=='E' || c=='I' ||c=='O' || c=='U') && ((c>='a'&& c<='z')||(c>='A'&& c<='Z'))){
+                count++;
+            }
+        }
+        return count;
+    }
+
 
 
     public static void main(String[] args) {
         String s1 = "decimaa";
         String s2 = "medical";
 
-//        System.out.println(length(s1));
-//
-//        System.out.println(toLower(s1));
-//
-//        System.out.println(toUpper(s1));
-//
+        System.out.println(length(s1));
+
+        System.out.println(toLower(s1));
+
+        System.out.println(toUpper(s1));
+
+        System.out.println(toggleCase(s1));
+
+        System.out.println(countVowels(s1));
+
+        System.out.println(countConsonants(s1));
+
     }
 }
