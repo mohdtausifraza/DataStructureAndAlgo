16168ae25171abeb0eed788224ebb6b99332e0fc
Tausif <49245808+mohdtausifraza@users.noreply.github.com>
2020-11-14 00:35:34 +0530

    SquareBandMatrix.

---
+++ DataStructure/src/main/java/Matrices/SquareBandMatrix.java
@@ -0,0 +1,19 @@
+package Matrices;
+
+/**
+ * Main Diagonal , Upper , and Lower Matrix are non zero
+ * Also having more than one Upper and Lower Diagonal Matrix
+ *              1 1 1 1 0 0 0 0
+ *              1 1 1 1 1 0 0 0
+ * M[i,j] =     1 1 1 1 1 1 0 0
+ *              1 1 1 1 1 1 1 0
+ *              0 1 1 1 1 1 1 1
+ *              0 0 1 1 1 1 1 1
+ *              0 0 0 1 1 1 1 1
+ *              0 0 0 0 1 1 1 1
+ *
+ *
+ */
+public class SquareBandMatrix {
+
+}
