b7257c77f4a2634f93f69fa3586b09b89ff1f40e
TausifRaza <write2tausifraza@gmail.com>
2021-10-12 22:58:49 +0530

    Doubly Linked List Delete and Reverse

--- DataStructure/src/main/java/LinkedList/DoublyLinkedList.java
+++ DataStructure/src/main/java/LinkedList/DoublyLinkedList.java
@@ -48,11 +48,89 @@ public class DoublyLinkedList {
         System.out.println("null");
     }
 
+    /**
+     * Inserting in DoublyLinkedList
+     * 1. Inserting at the first position
+     * 2. Inserting at the given index
+     */
+    public static ListNode insert(ListNode head , ListNode node ,int index){
+        if ((head == null && index != 1) || index <1){
+            System.out.println("Index out of bound");
+            return head;
+        }
+        else if (index == 1){
+            if (head != null) {
+                node.next = head;
+                head.prev = node;
+            }
+            head = node;
+        }else {
+            ListNode temp = head;
+            for (int i=1 ; i<index-1 && temp != null ; i++){
+                temp = temp.next;
+            }
+            if (temp==null){
+                System.out.println("Index out of bound");
+            }else {
+                node.next = temp.next;
+                temp.next = node;
+                node.prev = temp;
+                node.next.prev=node;
+            }
+        }
+        return head;
+    }
+
+    public  static ListNode delete(ListNode head, int index){
+        ListNode temp;
+        if (head == null || index < 1){
+            System.out.println("Index out of bound");
+            return  head;
+        }else if (index ==1){
+            temp = head;
+            head = head.next;
+            if(temp.next != null){
+                temp.next.prev=null;
+                temp.next = null;
+            }
+        }else{
+            temp = head;
+            for (int i=1; i< index-1 && temp!=null ;i++){
+                temp = temp.next;
+            }
+            if (temp == null || temp.next == null){
+                System.out.println("Index out of bound");
+            }else {
+                temp.next =  temp.next.next;
+                if (temp.next !=null) {
+                    temp.next.prev=temp;
+                }
+            }
+        }
+        temp=null;
+        return head;
+    }
+
+    /**
+     * Reversing DoublyLinkedList is same as rotating backward in a queue.
+     */
+    public static ListNode reverse(ListNode head){
+        ListNode temp = head;
+        while (temp != null){
+            temp = head.next;
+            head.next = head.prev;
+            head.prev = temp;
+            if (temp != null){
+                head = temp;
+            }
+        }
+        return head;
+    }
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
         ListNode doublyLinkedList = create(arr);
         display(doublyLinkedList);
-//        display(insert(doublyLinkedList , new ListNode(10),5));
+        display(insert(doublyLinkedList , new ListNode(10),5));
 //        ListNode deleteInDoublyList = delete(doublyLinkedList,10);
 //        display(deleteInDoublyList);
 //        reverseDisplay(deleteInDoublyList);
