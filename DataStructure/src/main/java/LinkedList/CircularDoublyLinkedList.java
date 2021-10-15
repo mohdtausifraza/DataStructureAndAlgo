891bc69b8a655a5bc495d93bc50b241f507271f2
TausifRaza <write2tausifraza@gmail.com>
2021-10-16 03:50:20 +0530

    CircularDoublyLinkedList Create

---
+++ DataStructure/src/main/java/LinkedList/CircularDoublyLinkedList.java
@@ -0,0 +1,36 @@
+package LinkedList;
+
+public class CircularDoublyLinkedList {
+    public static class ListNode{
+        int value;
+        ListNode prev;
+        ListNode next;
+        ListNode(int value){
+            this.value = value;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    public static ListNode create(int[] arr){
+        if (arr.length < 1){
+            return null;
+        }
+        ListNode head = new ListNode(arr[0]);
+        ListNode temp = head;
+        for (int i=1 ; i < arr.length ; i++){
+            temp.next = new ListNode(arr[i]);
+            temp.next.prev = temp;
+            temp = temp.next;
+        }
+        temp.next = head;
+        head.prev = temp;
+        return  head;
+    }
+
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6,7,8,9};
+        ListNode circularDLL = create(arr);
+    }
+}
