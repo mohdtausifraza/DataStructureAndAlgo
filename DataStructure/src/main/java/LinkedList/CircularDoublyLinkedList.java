68c2d6b224ab3f330ca0fb079703ff17ee6523a4
TausifRaza <write2tausifraza@gmail.com>
2021-10-17 01:10:29 +0530

    CircularDoublyLinkedList Display

--- DataStructure/src/main/java/LinkedList/CircularDoublyLinkedList.java
+++ DataStructure/src/main/java/LinkedList/CircularDoublyLinkedList.java
@@ -28,9 +28,25 @@ public class CircularDoublyLinkedList {
         return  head;
     }
 
+    public static  void display(ListNode head){
+        if (head == null){
+            System.out.println("Head->NULL");
+            return;
+        }
+        ListNode temp = head;
+        System.out.print("Head=>");
+        do{
+            System.out.print(temp.value + "<=>");
+            temp = temp.next;
+        }while (temp != head);
+        System.out.println("Head");
+
+    }
+
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9};
         ListNode circularDLL = create(arr);
+        display(circularDLL);
     }
 }
