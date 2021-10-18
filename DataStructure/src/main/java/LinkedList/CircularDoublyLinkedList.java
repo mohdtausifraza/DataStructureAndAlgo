e669e3520113b3a6d1c12144c71b470007e100dd
TausifRaza <write2tausifraza@gmail.com>
2021-10-18 21:32:38 +0530

    CircularDoublyLinkedList Insert

--- DataStructure/src/main/java/LinkedList/CircularDoublyLinkedList.java
+++ DataStructure/src/main/java/LinkedList/CircularDoublyLinkedList.java
@@ -43,10 +43,51 @@ public class CircularDoublyLinkedList {
 
     }
 
+    public  static ListNode insert(ListNode head, ListNode node, int index){
+        if (index < 1 || (head==null && index !=1)){
+            System.out.println("Index out of bound");
+        }else if (index == 1){
+            if (head == null){
+                head = node;
+                node.next=head;
+                node.prev=head;
+            }else{
+                ListNode temp = head.next;
+                while(temp.next != head){
+                    temp = temp.next;
+                }
+                node.next=head;
+                node.prev= head.prev;
+                temp.next = node;
+                head = node;
+            }
+        }else {
+            int flag =1;
+            ListNode temp = head;
+            for (int i=1 ; i < index-1; i++){
+                temp = temp.next;
+                if (temp == head){
+                    flag =0;
+                    break;
+                }
+            }
+            if (flag ==0){
+                System.out.println("Index out of bound");
+            }else{
+                node.next = temp.next;
+                node.prev = temp;
+                temp.next.prev = node;
+                temp.next = node;
+            }
+            temp = null;
+        }
+        return head;
+    }
 
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8,9};
         ListNode circularDLL = create(arr);
         display(circularDLL);
+        ListNode insert = insert(circularDLL, new ListNode(0),1);
     }
 }
