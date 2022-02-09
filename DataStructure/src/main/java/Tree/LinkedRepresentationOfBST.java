2c1eea38891bbf13d06aadb7f2fa17cc4861ed7f
TausifRaza <write2tausifraza@gmail.com>
2022-02-09 22:38:52 +0530

    Insert Iteratively in BST.

---
+++ DataStructure/src/main/java/Tree/LinkedRepresentationOfBST.java
@@ -0,0 +1,56 @@
+package Tree;
+
+import Stack.StackUsingLinkedList;
+
+import java.util.stream.IntStream;
+
+public class LinkedRepresentationOfBST {
+    public static class Node {
+        public Node leftChild;
+        public Node rightChild;
+        public int data;
+
+        public Node(int data) {
+            this.leftChild = null;
+            this.rightChild = null;
+            this.data = data;
+        }
+    }
+
+    /**
+     * Insert in a BST (Iterative)
+     * 1. Search for the keys in the tree with tail pointer
+     * 2. If the key is found don't insert
+     * 3. Else insert using tail pointer;
+     */
+    public Node insertIteratively(Node root, int key) {
+        if (root == null) {
+            return new Node(key);
+        } else {
+            Node tail = null, temp = root;
+            while (temp != null) {
+                tail = temp;
+                if (key == temp.data) {
+                    return root;
+                } else if (key < temp.data) {
+                    temp = temp.leftChild;
+                } else if (key > temp.data) {
+                    temp = temp.rightChild;
+                }
+            }
+            if (key < tail.data) {
+                tail.leftChild = new Node(key);
+            } else {
+                tail.rightChild = new Node(key);
+            }
+        }
+        return root;
+    }
+
+
+
+    public static void main(String[] args) {
+        LinkedRepresentationOfBST bst = new LinkedRepresentationOfBST();
+        int[] keys = {9, 15, 5, 20, 16, 8, 12, 3, 6};
+    }
+}
