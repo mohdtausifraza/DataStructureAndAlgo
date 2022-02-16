2436af9131df6eee930b3d442f3be2873522de5a
TausifRaza <write2tausifraza@gmail.com>
2022-02-16 23:18:29 +0530

    Delete From BST.

--- DataStructure/src/main/java/Tree/LinkedRepresentationOfBST.java
+++ DataStructure/src/main/java/Tree/LinkedRepresentationOfBST.java
@@ -159,8 +159,73 @@ public class LinkedRepresentationOfBST {
         }
         return node;
     }
+
+    /**
+     * Deleting from BST
+     * 1. Search for the Node. If found
+     * 2. If Node is not having any child delete it.
+     * 3. If Node is having only one child, delete the Node and let's it child takes his place
+     * 4. If Node is having two children,
+     * Find its Inorder Predecessor, and Inorder Successor
+     * If Predecessor or Successor is a leaf node, then that will take its place
+     * If Predecessor or Successor is Not a leaf node, then we have to make multiple modification.
+     * Modification = O(logn) - Height of the node
+     */
+    public Node delete(Node node, int key) {
+        Node temp = null;
+        if (node == null) {
+            return null;
+        }
+        if (node.leftChild==null && node.rightChild==null && key==node.data) {
+            return null;
+        }
+        if (key < node.data) {
+            node.leftChild = delete(node.leftChild, key);
+        } else if (key > node.data) {
+            node.rightChild = delete(node.rightChild, key);
+        } else {
+            if (height(node.leftChild) > height(node.rightChild)) {
+                temp = inorderPredecessor(node);
+                node.data = temp.data;
+                node.leftChild = delete(node.leftChild, temp.data);
+            } else {
+                temp = inorderSuccessor(node);
+                node.data = temp.data;
+                node.rightChild = delete(node.rightChild, temp.data);
+            }
+        }
+        return node;
+    }
     public static void main(String[] args) {
         LinkedRepresentationOfBST bst = new LinkedRepresentationOfBST();
         int[] keys = {9, 15, 5, 20, 16, 8, 12, 3, 6};
+        Node node = bst.createBST(keys);
+        node = bst.insertRecursively(node, 13);
+        node = bst.insertRecursively(node, 100);
+        System.out.print("Inorder Traversal :");
+        bst.inorder(node);
+        node = bst.delete(node, 3);
+        bst.inorder(node);
+        Node searchResult = bst.searchIterative(node, 15);
+        if (searchResult == null) {
+            System.out.println("\nElement is not found");
+        } else {
+            System.out.println("\nElement " + searchResult.data + " is found ");
+        }
+        System.out.println(bst.height(node));
+
+        Node predecessor = bst.inorderPredecessor(node);
+        if (predecessor != null) {
+            System.out.println("Inorder Predecessor for " + node.data + " is " + predecessor.data);
+        } else {
+            System.out.println("No Inorder Predecessor found for " + node.data);
+        }
+
+        Node successor = bst.inorderSuccessor(node);
+        if (successor != null) {
+            System.out.println("Inorder successor for " + node.data + " is " + successor.data);
+        } else {
+            System.out.println("No Inorder successor found for " + node.data);
+        }
     }
 }
